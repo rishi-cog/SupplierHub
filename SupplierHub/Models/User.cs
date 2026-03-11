9f3c5918ba77aea5c4464f9a816a96524bde9ad2
2478741 <2478741@LTIN718820>
2026-03-11 11:54:40 +0530

    .

---
+++ SupplierHub/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SupplierHub.Models;
+using SupplierHub.Constants.Enum;
+
+
+namespace SupplierHub.Models
+{
+	[Table("users")]
+	public class User
+	{
+		[Key]
+		public int UserId { get; set; }
+
+		[Required, MaxLength(120)]
+		public string Name { get; set; }
+
+		[Required, MaxLength(120)]
+		public string Email { get; set; }
+
+		[MaxLength(30)]
+		public string? Phone { get; set; }
+
+		[Required, MaxLength(60)]
+		public string Username { get; set; }
+
+		[Required]
+		public UserRole Role { get; set; }
+
+		[Required]
+		public UserStatus Status { get; set; }
+
+		[Required]
+		public byte[] PasswordHash { get; set; }
+
+		[Required]
+		public byte[] PasswordSalt { get; set; }
+
+		[Required]
+		public DateTime CreatedAtUtc { get; set; }
+
+		public DateTime? LastLoginAtUtc { get; set; }
+	}
+}
